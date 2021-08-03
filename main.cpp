1cb3c2f992ad06a6ea83e0547bc2a2c807197c55
Sean Lock <howlempyrean@gmail.com>
2021-08-03 14:09:28 +0200

    mergeSort

---
+++ main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+template <typename T>
+std::vector<T> mergeMeRee(const std::vector<T>& first, const std::vector<T>& second)
+{
+    std::vector<T> result;
+
+    std::deque<T> my_deq_first;
+
+    for (const auto& elem : first)
+    {
+        my_deq_first.emplace_back(std::move(elem));
+    }
+
+    std::deque<T> my_deq_second;
+
+    for (const auto& elem : second)
+    {
+        my_deq_second.emplace_back(std::move(elem));
+    }
+
+    while (!my_deq_first.empty() && !my_deq_second.empty())
+    {
+
+        if (my_deq_first.front() <= my_deq_second.front())
+        {
+            result.emplace_back(std::move(my_deq_first.front()));
+            my_deq_first.pop_front();
+        }
+        else
+        {
+            result.emplace_back(std::move(my_deq_second.front()));
+            my_deq_second.pop_front();
+        }
+    }
+
+    while (!my_deq_first.empty())
+    {
+        result.emplace_back(std::move(my_deq_first.front()));
+        my_deq_first.pop_front();
+    }
+
+    while (!my_deq_second.empty())
+    {
+        result.emplace_back(std::move(my_deq_second.front()));
+        my_deq_second.pop_front();
+    }
+    return result;
+}
+
+template <typename T>
+std::vector<T> mergeSort (const std::vector<T>& to_sort)
+{
+    if (to_sort.size() <= 1)
+    {
+        return to_sort;
+    }
+
+    std::vector<T> helper1;
+    std::vector<T> helper2;
+
+    auto reached_half = to_sort.size()/2;
+
+    for (int i = 0; i < to_sort.size(); ++i)
+    {
+        if (reached_half % 2 == 0)
+        {
+            if (i < (to_sort.size()/2))
+            {
+                helper1.emplace_back(to_sort[i]);
+            }
+            else
+            {
+                helper2.emplace_back(to_sort[i]);
+            }
+        }
+        else if (reached_half % 2 != 0)
+        {
+            reached_half = (to_sort.size()/2) + 1;
+
+            if (i < (to_sort.size()/2))
+            {
+                helper1.emplace_back(to_sort[i]);
+            }
+            else
+            {
+                helper2.emplace_back(to_sort[i]);
+            }
+        }
+    }
+
+    auto first_half = mergeSort((helper1));
+    auto second_half = mergeSort((helper2));
+
+    return mergeMeRee(first_half, second_half);
+}
+
+int main() {
+    std::vector<int> my_vec {5, 6, 3, 1, 10};
+    auto output = mergeSort(my_vec);
+
+    for (auto& elem : output)
+    {
+        std::cout << elem << ',';
+    }
+    return 0;
+}
